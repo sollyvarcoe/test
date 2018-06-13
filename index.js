c6facf87c0c3c410c27d5d949cee2647cbcb0279
Solomon Varcoe <sollyvarcoe@gmail.com>
2018-06-13 16:43:35 +0100

    commented

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ function Circle(x,y,dx,dy,radius) {
   this.fillColour = Math.floor(Math.random() * colourArray.length);
   this.draw = function() {
     c.beginPath();
-
     c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
     c.fillStyle = colourArray[this.fillColour]
     c.fill();
